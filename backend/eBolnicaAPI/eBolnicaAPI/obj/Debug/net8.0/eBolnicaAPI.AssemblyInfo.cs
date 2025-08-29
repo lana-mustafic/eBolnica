fea77efa4e0fe3fabea8d430b4dad2cc1c4701cd
Selms4 <selma.memic@edu.fit.ba>
2025-08-29 20:33:12 +0200

    Ignore obj/ folder and stop tracking generated AssemblyInfo

--- backend/eBolnicaAPI/eBolnicaAPI/obj/Debug/net8.0/eBolnicaAPI.AssemblyInfo.cs
+++ backend/eBolnicaAPI/eBolnicaAPI/obj/Debug/net8.0/eBolnicaAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eBolnicaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81733271f1a8231002d0287d3fd3c5b22cf44157")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6d46d3a7eb2902fb549ee8cd86edcf838048c03")]
 [assembly: System.Reflection.AssemblyProductAttribute("eBolnicaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eBolnicaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
